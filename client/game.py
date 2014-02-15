022453bde2804d6093d3f246466f67ed9d5e103c
RorschachUK <bob@bobstone.info>
2014-02-15 16:58:45 +0000

    Update game.py

--- client/game.py
+++ client/game.py
@@ -86,7 +86,7 @@ for ctrlid in config['local']['controls']:
                 GPIO.setup(pins['BTN_' + str(i)], GPIO.IN, GPIO.PUD_UP)
         elif hardwaretype == 'keypad': #four rows, four cols
             keypad = Keypad_BBB.keypad(pins['ROW_1'], pins['ROW_2'], pins['ROW_3'], pins['ROW_4'], 
-                                       pins['COL_1'], pins['COL_2'], pins['COL_3', pins['COL_4'])
+                                       pins['COL_1'], pins['COL_2'], pins['COL_3'], pins['COL_4'])
             
 #MQTT client
 client = mosquitto.Mosquitto("Game-" + ipaddress) #client ID
