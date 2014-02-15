16b2a569a120340f900284a3c3406f6c2b84e65d
RorschachUK <bob@bobstone.info>
2014-02-15 16:53:12 +0000

    Update game.py

--- client/game.py
+++ client/game.py
@@ -54,7 +54,7 @@ for ctrlid in config['local']['controls']:
             PWM.start(pins['RGB_B'], 0.0)
         elif hardwaretype == 'bargraphpotentiometer': #10k pot, 10 LEDs
             for barnum in range(11):
-                pin = pins['BAR_' + str(barnum+1)
+                pin = pins['BAR_' + str(barnum+1)]
                 GPIO.setup(pin, GPIO.OUT)
                 GPIO.output(pin, GPIO.HIGH)
                 bar.append(pin)
