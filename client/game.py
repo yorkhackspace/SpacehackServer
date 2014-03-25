7d0720253c87f3dcdd482453cda9a87d6a002a69
danieljabailey <danieljabailey@gmail.com>
2014-03-25 16:40:54 +0000

    autodeploy test

--- client/game.py
+++ client/game.py
@@ -29,6 +29,11 @@ ipaddress = commands.getoutput("/sbin/ifconfig").split("\n")[1].split()[1][5:]
 configFileName = 'game-' + ipaddress +'.config'
 config_manager.loadConfig(configFileName)
 
+config = config_manager.config              #Raw JSON config stored here
+controlids = config_manager.controlids     #IDs for all controls
+controldefs = config_manager.controldefs   #JSON config for each control
+sortedlist = config_manager.sortedlist     #TODO understand this! Bob, what is this for?
+
 #Vars
 #lcd={}
 roundconfig = {}
