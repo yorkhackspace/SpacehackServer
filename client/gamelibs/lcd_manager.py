ab477570b3d81d7636f80889d13d9fc68d51b3b2
danieljabailey <danieljabailey@gmail.com>
2014-03-25 16:31:09 +0000

    autodeploy test

---
+++ client/gamelibs/lcd_manager.py
@@ -0,0 +1,18 @@
+
+lcd={}
+
+def initLCDs(sortedlist, config):
+    for ctrlid in sortedlist:
+        dispdef = config['local']['controls'][ctrlid]['display']
+        if dispdef['type'] == 'hd44780':
+	        newlcd = Adafruit_CharLCD()
+	        newlcd.pin_e = dispdef['pin']
+	        GPIO.setup(newlcd.pin_e, GPIO.OUT)
+	        GPIO.output(newlcd.pin_e, GPIO.LOW)
+	        newlcd.begin(dispdef['width'], dispdef['height'])
+	        lcd[ctrlid]=newlcd
+	        print("Control " + ctrlid + " is hd44780 on pin " + newlcd.pin_e)
+        else:
+	        newlcd = NokiaLCD(pin_SCE=dispdef['pin'])
+	        lcd[ctrlid]=newlcd
+	        print("Control " + ctrlid + " is nokia on pin " + dispdef['pin'])
