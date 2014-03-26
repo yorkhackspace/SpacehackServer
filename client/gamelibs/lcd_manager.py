3402b8a167ace11e45b541e4e3e3f9a8174ce0f7
danieljabailey <danieljabailey@gmail.com>
2014-03-26 21:28:37 +0000

    autodeploy test

--- client/gamelibs/lcd_manager.py
+++ client/gamelibs/lcd_manager.py
@@ -34,14 +34,26 @@ def displayTimer(timeoutstarted, timeoutdisplayblocks, timeout):
     mytimeoutstarted = timeoutstarted
     mytimeoutdisplayblocks = timeoutdisplayblocks
 
+    print "mytimeoutstarted: "
+    print mytimeoutstarted
+    print "\nmytimeoutdisplayblocks: "
+    print mytimeoutdisplayblocks
+    print "\n"
+
     if mytimeoutstarted == 0.0:
         mytimeoutdisplayblocks = 0
     else:
         timesincetimeout = time.time() - mytimeoutstarted
+        print "timesincetimeout: "
+        print timesincetimeout
+        print "\n"
         if timesincetimeout > timeout:
             blockstodisplay = 0
         else:
             blockstodisplay = int(0.5 + 20 * (1 - (timesincetimeout / timeout)))
+        print "blockstodisplay: "
+        print blockstodisplay
+        print "\n"        
         #Work out diff between currently displayed blocks and intended, to minimise amount to draw
         if blockstodisplay > mytimeoutdisplayblocks:
             lcd["0"].setCursor(mytimeoutdisplayblocks, 3)
