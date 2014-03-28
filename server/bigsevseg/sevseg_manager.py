d786d4f80b6da8932f9966cb667941710550a85d
danieljabailey <danieljabailey@gmail.com>
2014-03-28 15:57:17 +0000

    autodeploy test

--- server/bigsevseg/sevseg_manager.py
+++ server/bigsevseg/sevseg_manager.py
@@ -5,18 +5,18 @@ import time
 print "imports done"
 sevenSeg = ExpIO.Adafruit_MCP230XX(0x20, 16)
 print "init done"
-for i in range(0, 15):
+for i in range(8, 15):
     sevenSeg.config(i, ExpIO.Adafruit_MCP230XX.OUTPUT)
 
 print "All pins set to output"
 
 while True:
     print "blink"
-    for i in range(0, 15):
+    for i in range(8, 15):
         sevenSeg.output(i, 1)    
         time.sleep(0.1)
     time.sleep(0.5)
-    for i in range(0, 15):
+    for i in range(8, 15):
         sevenSeg.output(i, 0)
         time.sleep(0.1)
     time.sleep(0.5)
