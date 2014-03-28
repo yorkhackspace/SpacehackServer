6ec551b3bb881d4bb88dd16a521958f5943e4d86
danieljabailey <danieljabailey@gmail.com>
2014-03-28 14:13:19 +0000

    autodeploy test

--- server/bigsevseg/sevseg_manager.py
+++ server/bigsevseg/sevseg_manager.py
@@ -7,9 +7,9 @@ for i in range(0 to 15):
     sevenSeg.config(i, ExpIO.Adafruit_MCP230XX.OUTPUT)
 
 while true:
-    for i in range(0 to 15):
+    for i in range(0, 15):
         sevenSeg.output(i, 1)    
     time.sleep(0.5)
-    for i in range(0 to 15):
+    for i in range(0, 15):
         sevenSeg.output(i, 0)
     time.sleep(0.5)
