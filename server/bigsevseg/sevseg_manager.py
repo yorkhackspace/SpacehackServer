abc4c83e385f03dd76e0c56e0ed369d73bbd12f0
danieljabailey <danieljabailey@gmail.com>
2014-03-28 14:12:32 +0000

    autodeploy test

---
+++ server/bigsevseg/sevseg_manager.py
@@ -0,0 +1,15 @@
+sys.path.append('./bigsevseg')
+import Adafruit_MCP230xx as ExpIO
+import time
+
+sevenSeg = ExpIO.Adafruit_MCP230XX(0x20, 16)
+for i in range(0 to 15):
+    sevenSeg.config(i, ExpIO.Adafruit_MCP230XX.OUTPUT)
+
+while true:
+    for i in range(0 to 15):
+        sevenSeg.output(i, 1)    
+    time.sleep(0.5)
+    for i in range(0 to 15):
+        sevenSeg.output(i, 0)
+    time.sleep(0.5)
