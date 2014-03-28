bf462f03d9adf42e113273300ef70e206253924a
danieljabailey <danieljabailey@gmail.com>
2014-03-28 15:53:05 +0000

    autodeploy test

--- server/bigsevseg/sevseg_manager.py
+++ server/bigsevseg/sevseg_manager.py
@@ -10,11 +10,13 @@ for i in range(0, 15):
 
 print "All pins set to output"
 
-while true:
+while True:
     print "blink"
     for i in range(0, 15):
         sevenSeg.output(i, 1)    
+        time.sleep(0.1)
     time.sleep(0.5)
     for i in range(0, 15):
         sevenSeg.output(i, 0)
+        time.sleep(0.1)
     time.sleep(0.5)
