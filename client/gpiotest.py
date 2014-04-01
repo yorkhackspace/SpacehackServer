bc6e79f487bb55d7120c9afe65ac61646561f0cd
danieljabailey <danieljabailey@gmail.com>
2014-04-01 16:12:19 +0100

    autodeploy test

---
+++ client/gpiotest.py
@@ -0,0 +1,23 @@
+import Adafruit_BBIO.GPIO as GPIO
+
+Run = True
+while (Run):
+	pin = input("Enter pin name or 'q' to exit: ");
+	InPin = True
+	if pin == "q":
+		Run = False
+	else:
+		while InPin:
+			Command = input("Select command; i-Input, o-Output, r-Read, 1-Set high, 0-Set low, b-Back: ")
+			if Command == "i":
+				GPIO.setup(pin, GPIO.IN, GPIO.PUD_DOWN)
+			elif Command == "o":
+				GPIO.setup(pin, GPIO.OUT)
+			elif Command == "r":
+				print GPIO.input(pin)
+			elif Command == "1":
+				GPIO.output(pin, GPIO.HIGH)
+			elif Command == "0":
+				GPIO.output(pin, GPIO.LOW)
+			elif Command == "b":
+				InPin = False
