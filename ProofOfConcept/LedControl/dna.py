0d3485142c5f698cbfa9d63c030dfd644cd715d0
RorschachUK <bob@bobstone.info>
2014-04-05 21:38:44 +0100

    Update dna.py

--- ProofOfConcept/LedControl/dna.py
+++ ProofOfConcept/LedControl/dna.py
@@ -1,3 +1,4 @@
+#Quick test of LedControl library
 import LedControl
 
 lc = LedControl.LedControl("P9_11", "P9_13", "P9_15", 1)
@@ -6,6 +7,3 @@ lc.setIntensity(0, 15)
 lc.clearDisplay(0)
 lc.setLed(0, 2, 3, 1)
 lc.setRow(0, 0, 127)
-while True:
-    #do nothing
-    x = 1
