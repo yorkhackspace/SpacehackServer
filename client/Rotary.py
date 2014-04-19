74edae858d7c1806cc197104913da85aad616d6d
danieljabailey <danieljabailey@gmail.com>
2014-04-19 20:22:27 +0100

    try to fix rot enc

--- client/Rotary.py
+++ client/Rotary.py
@@ -1,4 +1,3 @@
-import Adafruit_BBIO.GPIO as GPIO
 import Queue
 import threading
 from time import sleep
@@ -12,7 +11,7 @@ class RotaryEncoder(threading.Thread):
 events to the queue with either "cw" or "ccw"
 """
 
-    def __init__(self, queue, encoder_name, pins):
+    def __init__(self, queue, encoder_name, pins, gpio):
         """ Expects a queue to send events to
 and an array of the two input pins
 """
@@ -21,34 +20,25 @@ and an array of the two input pins
         self.pin_a = pins[0]
         self.pin_b = pins[1]
         self.encoder_name = encoder_name
+        self.gpio = gpio
         for pin in pins:
             print "Setting up pin: %s" % pin
-            GPIO.setup(pin, GPIO.IN, GPIO.PUD_OFF)
-            GPIO.add_event_detect(pin, GPIO.RISING)
+            self.gpio.setup(pin, self.gpio.IN, self.gpio.PUD_OFF)
+            self.gpio.add_event_detect(pin, self.gpio.RISING)
 
     def run(self):
         while True:
-            if GPIO.event_detected(self.pin_a):
+            if self.gpio.event_detected(self.pin_a):
                 # Ignore false triggers
-                if GPIO.input(self.pin_a) != 1:
+                if self.gpio.input(self.pin_a) != 1:
                     continue
                 level_b = 0
                 # De-bounce
                 for count in range(0,8):
-                    level_b += GPIO.input(self.pin_b)
+                    level_b += self.gpio.input(self.pin_b)
                     print(str(level_b))
                 if level_b > 5:
                     dir = "cw"
                 else:
                     dir = "ccw"
                 self.queue.put((self.encoder_name, dir))
-
-if __name__ == "__main__":
-    q = Queue.Queue()
-    enc = OpticalEncoder(q, "Flux Matrix", ["P8_22", "P8_24"])
-    enc.setDaemon(True)
-    enc.start()
-
-    while True:
-        state = q.get()
-        print state
