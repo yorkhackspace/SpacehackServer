54c086e5c52f1fe8816de80237ea2b50547502a0
danieljabailey <danieljabailey@gmail.com>
2014-04-19 21:37:26 +0100

    optical test

--- ProofOfConcept/events/optical_external.py
+++ ProofOfConcept/events/optical_external.py
@@ -1,6 +1,6 @@
 import Adafruit_BBIO.GPIO as GPIO
 import Queue
-import optical
+from optical import OpticalEncoder
 q = Queue.Queue()
 enc = OpticalEncoder(q, "Flux Matrix", ["P8_22", "P9_31"], GPIO)
 enc.setDaemon(True)
