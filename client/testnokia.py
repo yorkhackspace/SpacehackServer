4a80bcc8a030fafe566903133074920f16cced7f
danieljabailey <danieljabailey@gmail.com>
2014-04-20 16:42:13 +0100

    fixing nokias...

--- client/testnokia.py
+++ client/testnokia.py
@@ -9,31 +9,40 @@ import Adafruit_BBIO.GPIO as GPIO
 
 CE_0="P9_11"
 CE_1="P9_13"
+CE_2="P9_15"
 GPIO.setup(CE_0, GPIO.OUT)
 GPIO.setup(CE_1, GPIO.OUT)
+GPIO.setup(CE_2, GPIO.OUT)
 
 pc.init()
 
 GPIO.output(CE_0, GPIO.LOW)
 GPIO.output(CE_1, GPIO.LOW)
+GPIO.output(CE_2, GPIO.LOW)
 
 pc.screenInit()
 
 GPIO.output(CE_1, GPIO.HIGH)
+GPIO.output(CE_2, GPIO.HIGH)
 
 raw_input("sdf")
 f=0
 while True:
-    GPIO.output(CE_0, GPIO.HIGH)
+    GPIO.output(CE_0, GPIO.LOW)
     time.sleep(0.1)
-    pc.text("John is Ace!")
+    pc.text("LCD 0")
     pc.text("%s" % f )
     f  = f + 1
     time.sleep(0.1)
-    GPIO.output(CE_0, GPIO.LOW)
+    GPIO.output(CE_0, GPIO.HIGH)
+    GPIO.output(CE_1, GPIO.LOW)
+    time.sleep(0.1)
+    pc.text("LCD 1")
+    time.sleep(0.1)
     GPIO.output(CE_1, GPIO.HIGH)
     time.sleep(0.1)
-    pc.text("HackSpace!")
+    GPIO.output(CE_2, GPIO.LOW)
     time.sleep(0.1)
-    GPIO.output(CE_1, GPIO.LOW)
-    # time.sleep(0.1)
+    pc.text("LCD 2")
+    time.sleep(0.1)
+    GPIO.output(CE_2, GPIO.HIGH)
