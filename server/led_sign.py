0eb5d02221bb55b8da53338dfed930f1537a3699
danieljabailey <danieljabailey@gmail.com>
2014-04-21 14:45:30 +0100

    server fixes

---
+++ server/led_sign.py
@@ -0,0 +1,66 @@
+from Adafruit_I2C import Adafruit_I2C
+import time
+Addr = 0x04
+
+REG_Mode = ord('M')
+REG_Code1 = ord('1')
+REG_Code2 = ord('2')
+REG_FlashFreq = ord('F')
+
+CODE_Col_Green = ord('g')
+CODE_Col_Red = ord('r')
+CODE_Col_Red1 = ord('i')
+CODE_Col_Red2 = ord('o')
+CODE_Col_Red3 = ord('p')
+CODE_Col_Yellow = ord('y')
+
+MODE_Solid = ord('s')
+MODE_Flash = ord('f')
+MODE_Off = ord('o')
+
+i2c = Adafruit_I2C(address=Addr)
+
+#time.sleep(10)
+##Set the mode register to flash
+#i2c.write8(REG_Mode, MODE_Flash)
+#time.sleep(0.1)
+##set the first code to the colour Yellow
+#i2c.write8(REG_Code1, CODE_Col_Yellow)
+#time.sleep(0.1)
+##set the second code to the colour Red
+#i2c.write8(REG_Code2, CODE_Col_Red)
+#time.sleep(0.1)
+##flash frequency register set as delay in ms divided by four, minus 20. The number must be positive
+##flash speed 0 is fastest 0*4+20 = period of 20ms = 50Hz
+##flash speed 120 is medium 120*4+20 = period of 500ms = 2Hz
+##flash speed 245 is slow 245*4+20 = period of 1000ms = 1Hz
+#i2c.write8(REG_FlashFreq, 120)
+
+##Sign is now 2Hz red and yellow flash
+
+def solid(solid_code):
+    #Set the mode register to solid
+    i2c.write8(REG_Mode, MODE_Solid)
+    time.sleep(0.1)
+    #set the solid code
+    i2c.write8(REG_Code1, solid_code)
+    
+def flash(code1, code2, period):
+    #Set the mode register to flash
+    i2c.write8(REG_Mode, MODE_Flash)
+    time.sleep(0.1)
+    #set the first code
+    i2c.write8(REG_Code1, code1)
+    time.sleep(0.1)
+    #set the second code
+    i2c.write8(REG_Code2, code2)
+    time.sleep(0.1)
+    #flash frequency register set as delay in ms divided by four, minus 20. The number must be positive
+    #flash speed 0 is fastest 0*4+20 = period of 20ms = 50Hz
+    #flash speed 120 is slow 120*4+20 = period of 500ms = 2Hz
+    i2c.write8(REG_FlashFreq, period)
+    
+def off():
+    #Set the mode register to off
+    i2c.write8(REG_Mode, MODE_Off)
+
---
+++ server/seven_segment_display.py
@@ -0,0 +1,55 @@
+import sys
+sys.path.append('./bigsevseg')
+import Adafruit_MCP230xx as ExpIO
+import time
+sevenSeg = ExpIO.Adafruit_MCP230XX(0x20, 16)
+
+#    A
+#    _
+#F  |_| B
+#E  |_| C
+#
+#    D
+#
+#Centre: G
+
+#seg = dict(A = 7, B = 9, C = 10, D = 11, E = 12, F = 13, G = 14)
+
+seg = dict(A = 13, B = 9, C = 12, D = 14, E = 7, F = 11, G = 10)
+
+digit_1 = [     'B', 'C'                    ]
+digit_2 = ['A', 'B',      'D', 'E',      'G']
+digit_3 = ['A', 'B', 'C', 'D',           'G']
+digit_4 = [     'B', 'C',           'F', 'G']
+digit_5 = ['A',      'C', 'D',      'F', 'G']
+digit_6 = ['A',      'C', 'D', 'E', 'F', 'G']
+digit_7 = ['A', 'B', 'C'                    ]
+digit_8 = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+digit_9 = ['A', 'B', 'C', 'D',      'F', 'G']
+digit_0 = ['A', 'B', 'C', 'D', 'E', 'F'     ]
+
+test_1 = ['A']
+test_2 = ['B']
+test_3 = ['C']
+test_4 = ['D']
+test_5 = ['E']
+test_6 = ['F']
+test_7 = ['G']
+
+digits = [digit_0, digit_1, digit_2, digit_3, digit_4, digit_5, digit_6, digit_7, digit_8, digit_9]
+tests = [test_1, test_2, test_3, test_4, test_5, test_6, test_7]
+
+for i in seg:
+    sevenSeg.config(seg[i], ExpIO.Adafruit_MCP230XX.OUTPUT)
+
+def clear():
+    for i in seg:
+        sevenSeg.output(seg[i], 0)
+
+def digit(digit):
+    for s in digit:
+        sevenSeg.output(seg[s], 1)
+        
+def displayDigit(num):
+    clear()
+    digit(digits[num])
