b20744f8fb0ed589b28dfa84b893e7456f062f47
danieljabailey <danieljabailey@gmail.com>
2014-04-21 12:39:31 +0100

    trying LED sign

--- server/bigsevseg/led_sign.py
+++ server/bigsevseg/led_sign.py
@@ -19,28 +19,49 @@ MODE_Flash = ord('f')
 MODE_Off = ord('o')
 
 i2c = Adafruit_I2C(address=Addr)
-#Set the mode register to solid
-i2c.write8(REG_Mode, MODE_Solid)
-time.sleep(0.1)
-#set the solid code to the colour green
-i2c.write8(REG_Code1, CODE_Col_Green)
 
-#sign is now solid green
+#time.sleep(10)
+##Set the mode register to flash
+#i2c.write8(REG_Mode, MODE_Flash)
+#time.sleep(0.1)
+##set the first code to the colour Yellow
+#i2c.write8(REG_Code1, CODE_Col_Yellow)
+#time.sleep(0.1)
+##set the second code to the colour Red
+#i2c.write8(REG_Code2, CODE_Col_Red)
+#time.sleep(0.1)
+##flash frequency register set as delay in ms divided by four, minus 20. The number must be positive
+##flash speed 0 is fastest 0*4+20 = period of 20ms = 50Hz
+##flash speed 120 is medium 120*4+20 = period of 500ms = 2Hz
+##flash speed 245 is slow 245*4+20 = period of 1000ms = 1Hz
+#i2c.write8(REG_FlashFreq, 120)
 
-time.sleep(10)
-#Set the mode register to flash
-i2c.write8(REG_Mode, MODE_Flash)
-time.sleep(0.1)
-#set the first code to the colour Yellow
-i2c.write8(REG_Code1, CODE_Col_Yellow)
-time.sleep(0.1)
-#set the second code to the colour Red
-i2c.write8(REG_Code2, CODE_Col_Red)
-time.sleep(0.1)
-#flash frequency register set as delay in ms divided by four, minus 20. The number must be positive
-#flash speed 0 is fastest 0*4+20 = period of 20ms = 50Hz
-#flash speed 120 is medium 120*4+20 = period of 500ms = 2Hz
-#flash speed 245 is slow 245*4+20 = period of 1000ms = 1Hz
-i2c.write8(REG_FlashFreq, 120)
+##Sign is now 2Hz red and yellow flash
 
-#Sign is now 2Hz red and yellow flash
+def LED_sign_solid(solid_code):
+    #Set the mode register to solid
+    i2c.write8(REG_Mode, MODE_Solid)
+    time.sleep(0.1)
+    #set the solid code
+    i2c.write8(REG_Code1, solid_code)
+    
+def LED_sign_flash(code1, code2, period):
+    #Set the mode register to flash
+    i2c.write8(REG_Mode, MODE_Flash)
+    time.sleep(0.1)
+    #set the first code
+    i2c.write8(REG_Code1, code1)
+    time.sleep(0.1)
+    #set the second code
+    i2c.write8(REG_Code2, code2)
+    time.sleep(0.1)
+    #flash frequency register set as delay in ms divided by four, minus 20. The number must be positive
+    #flash speed 0 is fastest 0*4+20 = period of 20ms = 50Hz
+    #flash speed 120 is medium 120*4+20 = period of 500ms = 2Hz
+    #flash speed 245 is slow 245*4+20 = period of 1000ms = 1Hz
+    i2c.write8(REG_FlashFreq, period)
+    
+def LED_sign_off():
+    #Set the mode register to off
+    i2c.write8(REG_Mode, MODE_Off)
+
