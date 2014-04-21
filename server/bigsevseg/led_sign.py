413ad2b4d7b9bbd70852ed834bb8aad0b029acd2
danieljabailey <danieljabailey@gmail.com>
2014-04-21 10:24:04 +0100

    trying LED sign

--- server/bigsevseg/led_sign.py
+++ server/bigsevseg/led_sign.py
@@ -1,7 +1,5 @@
 from Adafruit_I2C import Adafruit_I2C
 Addr = 0x04
 i2c = Adafruit_I2C(address=Addr)
-i2c.write8(0, ord('M'))
-i2c.write8(1, ord('s'))
-i2c.write8(2, ord('1'))
-i2c.write8(3, ord('g'))
+i2c.write8(ord('M'), ord('s'))
+i2c.write8(ord('1'), ord('g'))
