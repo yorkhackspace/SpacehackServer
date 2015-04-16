2a26a83afc18b025863dbd374a19b75687f8e9ad
danieljabailey <danieljabailey@gmail.com>
2015-04-16 21:12:46 +0000

    corrected pot calibration for console 4

--- client/pot_calib.py
+++ client/pot_calib.py
@@ -1,7 +1,8 @@
 import Adafruit_BBIO.ADC as ADC
 import time
 #37, 38, 39, 40
-ADC.setup("P9_40")
+pin = "P9_39"
+ADC.setup(pin)
 while True:
-  print ADC.read("P9_40")
+  print ADC.read(pin)
   time.sleep (0.0100)
